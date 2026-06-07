2aaf14bdef758abce153d12fd13ed47997cc4089
JP White <jpwhite3@gmail.com>
2026-06-07 13:54:33 -0400

    Fix PWA subfolder caching collision with navigateFallbackDenylist

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
         navigateFallback: 'index.html',
+        navigateFallbackDenylist: [/^\/[^\/]+\/(?!(assets|icons|favicon\.ico|manifest\.webmanifest|sw\.js|registerSW\.js)($|\/))[^\/]+/],
         cleanupOutdatedCaches: true,
       },
     }),
