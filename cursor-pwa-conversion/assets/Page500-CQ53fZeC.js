8fe4a7a19a9b67a9fcc2bcfa2c66b795ac1bd237
jpwhite3 <jpwhite3@users.noreply.github.com>
2026-06-07 16:26:55 +0000

    Deploying to gh-pages from @ jpwhite3/countdown-timer-app@2badb4bae2b6ffb9eb3aba402b6ed0b22ced125e 🚀

--- cursor-pwa-conversion/assets/Page500-CQ53fZeC.js
+++
@@ -1,1 +0,0 @@
-import{i as e,t}from"./index-BtTNYnTs.js";e();var n=t(),r=()=>(0,n.jsxs)(`div`,{className:`m-4 text-center`,children:[(0,n.jsx)(`h1`,{className:`display-3`,children:`500`}),(0,n.jsx)(`p`,{className:`fs-4`,children:`Something went wrong on our end. Please try again later.`})]});export{r as default};
