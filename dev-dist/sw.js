bb194bd026e4718300a65af4de993df3580e0f34
JP White <jpwhite3@gmail.com>
2026-06-07 13:00:53 -0400

    ci(pages): fix target-folder expression for main branch deployment to root

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k02ujj07h08"
+    "revision": "0.vt5ikjj9vvs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
